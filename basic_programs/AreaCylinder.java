78c1f9aef073bac06e5bc4ac8160dc688f31dfee
unknown <you@example.com>
2026-01-11 01:12:19 +0530

    Assignment 5

---
+++ basic_programs/AreaCylinder.java
@@ -0,0 +1,12 @@
+import java.util.Scanner ;
+public class AreaCylinder{
+	public static void main(String[] args )  {
+		Scanner sc =  new Scanner(System.in)  ;
+		System.out.println("Enter The Radius ");
+		 double radius = sc.nextDouble();
+		 System.out.println("Enter The Height");
+		 double height = sc.nextDouble();
+		 double area   =  3.14 * (radius * radius) * height;
+		 System.out.printf("The area of cylinder is %.2f%n " , area);
+	}
+}

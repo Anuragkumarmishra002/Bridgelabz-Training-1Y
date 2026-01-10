752ec08d4d842cfbd8a607dad354058210025421
unknown <you@example.com>
2026-01-11 01:15:25 +0530

    Assignment 5

--- basic_programs/Cylinder.java
+++ basic_programs/Cylinder.java
@@ -1,5 +1,5 @@
 import java.util.Scanner ;
-public class Cylinder{
+public class AreaCylinder{
 	public static void main(String[] args )  {
 		Scanner sc =  new Scanner(System.in)  ;
 		System.out.println("Enter The Radius ");

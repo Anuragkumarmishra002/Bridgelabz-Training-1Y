f686427bf7a4e4ab4c254e18f57ddb5cab0a58bd
unknown <you@example.com>
2026-01-11 01:10:46 +0530

    Assignment 4

---
+++ basic_programs/Circle.java
@@ -0,0 +1,10 @@
+import java.util.Scanner ;
+public class Circle{
+	public static void main(String[] args )  {
+		Scanner sc =  new Scanner(System.in)  ;
+		System.out.println("Enter The Radius ");
+		 float radius = sc.nextFloat();
+		 double area   =  3.14 * radius * radius ;
+		 System.out.println("The area of circle is " +area);
+	}
+}

a98edd2a48dcc47c8a17e062b1751d1c6c9b3258
unknown <you@example.com>
2026-01-15 22:45:12 +0530

    Level 2 Assignment 2 Problem 7

---
+++ basic_programs/IntOperation.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class IntOperation{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+System.out.print("Enter a , b, c: ");
+int a=sc.nextInt();
+int b=sc.nextInt();
+int c=sc.nextInt();
+int r1=a+b*c;
+int r2=a*b+c;
+int r3=c+a/b;
+int r4=a%b+c;
+System.out.println("Results are: "+r1+", "+r2+", "+r3+", "+r4);
+}
+}

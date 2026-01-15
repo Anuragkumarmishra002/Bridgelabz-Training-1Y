df09f65ceaa004888025996430eb899497aab33d
unknown <you@example.com>
2026-01-15 13:35:43 +0530

    Assignment 2 problem 2

---
+++ basic_programs/Marks.java
@@ -0,0 +1,11 @@
+
+public class Marks {
+public static void main(String[] args ) {
+double mathsNum = 94 ;
+double physicsNum = 95 ;
+double chemistryNum = 96 ;
+double averagePercentage = (mathsNum + physicsNum +chemistryNum )/100;
+System.out.printf("Sam’s average mark in PCM is %.2f" , averagePercentage ) ;
+}
+}
+

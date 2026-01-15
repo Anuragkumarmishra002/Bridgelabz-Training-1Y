fb04eca6b41d67b12ee2c10056ffbcb4ed68841b
unknown <you@example.com>
2026-01-15 13:33:18 +0530

    Level 1 Assigmnent 2 problem 1

---
+++ basic_programs/Age.java
@@ -0,0 +1,9 @@
+
+public class Age{
+public static void main(String[] args ) {
+int birthHarry = 2000 ;
+int  currentYear = 2024 ;
+int currentAge = currentYear -  birthHarry ;
+System.out.print("Harry's age in 2024 is " + currentAge ) ;
+}
+}

69ddf2db1b3db5bf7f90f85fbe8727013c1c9bf6
unknown <you@example.com>
2026-01-15 13:43:05 +0530

    Assignment 2 problem 5

---
+++ basic_programs/EqualityPen.java
@@ -0,0 +1,10 @@
+
+public class EqualityPen {
+public static void main(String[] args ) {
+int pen = 14 ;
+int student = 3 ;
+int divide = pen /  student ;
+int remaining = pen % student ;
+System.out.printf("The Pen Per Student is %d and the remaining pen not distributed is %d " , divide , remaining ) ;
+}
+}

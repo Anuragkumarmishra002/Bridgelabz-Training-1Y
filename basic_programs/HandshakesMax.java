0228b3d3e4073ad8922c9439b6a4461fb25cc7de
unknown <you@example.com>
2026-01-15 21:51:50 +0530

    Assignment 2 Problem 16

---
+++ basic_programs/HandshakesMax.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class HandshakesMax{
+public static void main(String[] args){
+Scanner input = new Scanner(System.in);
+int n= input.nextInt();
+int handshakes = (n*(n-1))/2;
+System.out.println("The maximum number of handshakes is "+handshakes);
+}
+}

942c3f1d7512e89208d8b42878cd99f3898f0302
unknown <you@example.com>
2026-01-15 13:38:22 +0530

    Assignment 2 problem 3

---
+++ basic_programs/Distance.java
@@ -0,0 +1,8 @@
+
+public class Distance {
+public static void main(String[] args ) {
+double km = 10.8 ;
+double  miles  =  10.8 * 1.6 ;
+System.out.printf("The distance %.2f km in miles is %.2f " , km , miles ) ;
+}
+}

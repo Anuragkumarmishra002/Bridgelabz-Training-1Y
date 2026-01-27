31be8037d6a44ecbb3da8078f636e4758badbae1
unknown <you@example.com>
2026-01-27 23:44:05 +0530

    Assignment 3 Level 3 Problem 5

---
+++ basic_programs/ArmstrongNumber.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class ArmstrongNumber {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+        int originalNumber = number;
+        int sum = 0;
+        while (originalNumber != 0) {
+            int digit = originalNumber % 10;
+            sum = sum + (digit * digit * digit);
+            originalNumber = originalNumber / 10;
+        }
+        if (sum == number) {
+            System.out.println("Number is an Armstrong Number");
+        } else {
+            System.out.println("Number is not an Armstrong Number");
+        }
+    }
+}

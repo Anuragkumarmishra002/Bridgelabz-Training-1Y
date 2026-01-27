e602630f05e5e49041a5653c337ffe54a28ab6c8
unknown <you@example.com>
2026-01-28 02:30:48 +0530

    Assignment 3 Level 3 Problem 9

---
+++ basic_programs/AbundantNumber.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class AbundantNumber{
+public static void main(String[] args){
+Scanner input =new Scanner(System.in);
+System.out.print("Enter the number ");
+int num= input.nextInt();
+int sum=0;
+for(int i=1;i<num;i++){
+if(num%i==0){
+sum=sum+i;
+}
+}
+if(sum>num){
+System.out.println("Abundant Number");
+}else{
+System.out.println("Not an Abundant Number");
+}
+}
+}

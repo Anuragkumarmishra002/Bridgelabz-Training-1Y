a94ac49866d8ba2ecc2d43bfb405b42893fc301f
Anuragkumarmishra002 <mishraanu55512@gmail.com>
2026-02-11 22:43:19 +0530

    Strings Assignment

--- src/com/gla/arrays/Anurag.java
+++
@@ -1,7 +0,0 @@
-package com.gla.arrays;
-
-public class Anurag {
-    public static void main(String[] args) {
-        System.out.print("Hello");
-    }
-}
--- src/com/gla/arrays/AnuragDemo2.java
+++
@@ -1,7 +0,0 @@
-package com.gla.arrays;
-
-public class AnuragDemo2 {
-    public static void main(String[] args) {
-        System.out.print("Hello Demo 2 ");
-    }
-}

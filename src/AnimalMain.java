4081a2b7a74db7b826de4b98e6209aaac91cb51c
Anuragkumarmishra002 <mishraanu55512@gmail.com>
2026-04-03 07:15:39 +0530

    Inheritance And Polymorphisim Assignment

---
+++ src/AnimalMain.java
@@ -0,0 +1,45 @@
+
+class Animal{
+    String name;
+    int age;
+    Animal(String name,int age){
+        this.name=name;
+        this.age=age;
+    }
+    void makeSound(){
+        System.out.println("Animal makes sound");
+    }
+}
+class Dog extends Animal{
+    Dog(String name,int age){
+        super(name,age);
+    }
+    @Override
+    void makeSound(){
+        System.out.println("Dog barks");
+    }
+}
+class Cat extends Animal{
+    Cat(String name,int age){
+        super(name,age);
+    }
+    @Override
+    void makeSound(){
+        System.out.println("Cat meows");
+    }
+}
+class Bird extends Animal{
+    Bird(String name,int age){
+        super(name,age);
+    }
+    @Override
+    void makeSound(){
+        System.out.println("Bird chirps");
+    }
+}
+public class AnimalMain {
+    public static void main(String[]args){
+        Animal[]arr={new Dog("Tiger ",3),new Cat("Kitty",2),new Bird("Tweety",1)};
+        for(Animal a:arr)a.makeSound();
+    }
+}
